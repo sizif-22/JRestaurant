abb9e4f784d7bd62c7a8dd002f2c84167ef65294
Sherif <127263385+sizif-22@users.noreply.github.com>
2025-08-12 03:38:36 +0300

    update the main class

--- src/main/java/com/jrestaurant/Main.java
+++ src/main/java/com/jrestaurant/Main.java
@@ -1,12 +1,20 @@
 package com.jrestaurant;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 // import com.jrestaurant.config.DatabaseConfig;
 
 public class Main {
 
+	public static int swap(int a, int b) {
+		return a;
+	};
+
 	public static void main(String[] args) {
 		System.out.println("test");
 		// DatabaseConfig dbconfig = new DatabaseConfig();
+
 	}
 
 }
